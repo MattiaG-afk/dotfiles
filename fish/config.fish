15b1bde893c3c36328d34f827af53c6daa84de69
gasmat <53443116+MattiaG-afk@users.noreply.github.com>
2021-10-16 17:41:15 +0200

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,12 @@ function fish_greeting
 	neofetch
 end
 
+# If running from tty1 start sway
+set TTY1 (tty)
+if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
+  exec sway
+end
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+	# Commands to run in interactive sessions can go here
 end
